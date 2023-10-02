300aca0b463ef399f77d4b7abe0f25fc634aaf89
mahdiosman2023 <mahdiosmanu78@gmail.com>
2023-10-03 00:16:55 +0300

    git@github.com:mahdiosman2023/phase-1-arithmetic-lab.git

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = 1
+Math.floor(Math.random() * 10) ; 
+
+let num3 = 4
+let num4 = 1
+let mod = num3 / num4
+
+let max = 20 
+
 
 // Write your code here
